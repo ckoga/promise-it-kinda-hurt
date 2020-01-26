6f3cafe144584cca25c3996ea0d8c1e18c83a249
Colin Turing <colin.koga@gmail.com>
2020-01-26 15:02:53 -0700

    Add promise functionality

--- solutions.js
+++ solutions.js
@@ -1,6 +1,44 @@
-
-setTimeout(() => {
-  console.log('TIMED OUT!')
-}, 300)
+require('es6-promise');
 
 
+//** Warm Up **//
+
+// setTimeout(() => {
+//   console.log('TIMED OUT!')
+// }, 300)
+
+
+//** Fuffil a promise **/
+
+// var promise = new Promise((fuffil, reject) => {
+//   setTimeout(() => {
+//     fuffil('FUFFILED!')
+//   }, 300);
+// })
+
+// promise.then(() => {
+//   console.log('FULFILLED!')
+// })
+
+//** Reject a Promise **//
+
+var promise = new Promise((fuffil, reject) => {
+  let num;
+  setTimeout(() => {
+    num = 2
+  }, 300);
+
+  if(num > 3) {
+    fuffil('The humber is greater than 3');
+  } else {
+    reject(Error('REJECTED!'))
+  }
+})
+
+const onReject = (error) => {
+  console.log(error.message)
+};
+
+promise.then(() => {}, (error) => {
+  onReject(error)
+})
